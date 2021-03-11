9d4fae7fc835ddc16e4867b2da3ab6d8521f0a36
Patryk Makowski <patryk.makowski23@gmail.com>
2021-03-11 21:03:06 +0100

    Add creating user

--- server/controllers/user-ctrl.js
+++ server/controllers/user-ctrl.js
@@ -24,3 +24,36 @@ getUserById = async (req, res) => {
     return res.status(200).json({ success: true, data: user });
   }).catch((err) => console.log(err));
 };
+
+createUser = (req, res) => {
+  const body = req.body;
+
+  if (!body) {
+    return res.status(400).json({
+      success: false,
+      error: "You must provide a User",
+    });
+  }
+
+  const user = new User(body);
+
+  if (!user) {
+    return res.status(400).json({ success: false, error: err });
+  }
+
+  user
+    .save()
+    .then(() => {
+      return res.status(201).json({
+        success: true,
+        id: user._id,
+        message: "User created!",
+      });
+    })
+    .catch((error) => {
+      return res.status(400).json({
+        error,
+        message: "User not created!",
+      });
+    });
+};
