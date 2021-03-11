3174790c3744e9b4452f6fbe930c60859c6e3c6b
Patryk Makowski <patryk.makowski23@gmail.com>
2021-03-11 21:03:41 +0100

    Add deleting user

--- server/controllers/user-ctrl.js
+++ server/controllers/user-ctrl.js
@@ -99,3 +99,17 @@ updateUser = async (req, res) => {
       });
   });
 };
+
+deleteUser = async (req, res) => {
+  await User.findOneAndDelete({ _id: req.params.userId }, (err, user) => {
+    if (err) {
+      return res.status(400).json({ success: false, error: err });
+    }
+
+    if (!user) {
+      return res.status(404).json({ success: false, error: `User not found` });
+    }
+
+    return res.status(200).json({ success: true, data: user });
+  }).catch((err) => console.log(err));
+};
