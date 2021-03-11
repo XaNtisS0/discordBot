d9bb1e4a3032db12c1ba295123dee8e0a0be7e44
Patryk Makowski <patryk.makowski23@gmail.com>
2021-03-11 21:02:12 +0100

    Add getting single user by id

--- server/controllers/user-ctrl.js
+++ server/controllers/user-ctrl.js
@@ -11,3 +11,16 @@ getUsers = async (req, res) => {
     return res.status(200).json({ success: true, data: Users });
   }).catch((err) => console.log(err));
 };
+
+getUserById = async (req, res) => {
+  await User.findOne({ _id: req.params.userId }, (err, user) => {
+    if (err) {
+      return res.status(400).json({ success: false, error: err });
+    }
+
+    if (!user) {
+      return res.status(404).json({ success: false, error: `User not found` });
+    }
+    return res.status(200).json({ success: true, data: user });
+  }).catch((err) => console.log(err));
+};
