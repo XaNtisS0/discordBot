4c9953e1378096b1c2083dccbcaf08a667cac302
Patryk Makowski <patryk.makowski23@gmail.com>
2021-03-11 21:00:38 +0100

    Add usage of newUrlParser, createIndex and UnifiedTopology

--- server/db/index.js
+++ server/db/index.js
@@ -2,9 +2,18 @@ const mongoose = require("mongoose");
 
 const connectionString = "mongodb://mongo:27017/bot";
 
-mongoose.connect(connectionString, { userNewUrlParser: true }).catch((err) => {
-  console.log("DB connection errror", e.message);
-});
+mongoose
+  .connect(connectionString, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    console.log("DB connection successful");
+  })
+  .catch((err) => {
+    console.log("DB connection errror", err.message);
+  });
 
 const db = mongoose.connection;
 
