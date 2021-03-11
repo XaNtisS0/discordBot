5d98df4018aff420c295c9e0a26c6d8a25b27fa2
Patryk Makowski <patryk.makowski23@gmail.com>
2021-03-11 20:59:31 +0100

    small refactor

--- server/models/user-model.js
+++ server/models/user-model.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
 
-const User = new Schema({
+const User = mongoose.Schema({
   username: { type: String, required: true },
   server_id: { type: Number, required: true },
   ranks: { type: [String], required: false },
