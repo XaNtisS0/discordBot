b4a7654c2ea2da013dbd634032d4bcbf8058eb1d
Patryk Makowski <patryk.makowski23@gmail.com>
2021-03-13 22:52:11 +0100

    Add api validation

--- bot/bot.py
+++ bot/bot.py
@@ -44,7 +44,10 @@ async def on_member_join(member):
     admin_role = discord.utils.get(guild.roles, name='members-admin')
     # Get api/servers/
     get_servers = requests.get(f'{api_server}/api/servers/').json()
-    print(get_servers)
+    if (get_servers['success'] == False):
+        print(get_servers['error'])
+        return 0
+    get_servers = get_servers['data']
     # Get channel with name="bot-log" for logging
     logging_channel = discord.utils.get(guild.channels, name="bot-log")
     if not logging_channel:
@@ -57,15 +60,23 @@ async def on_member_join(member):
             server = get_servers[i]
     if server == {}:
         await logging_channel.send(f'{admin_role.mention} go to {webpage} to add server to bot database.')
+        return 0
     # Get api/{server_id}/users
     server_id = server['_id']
     get_users = requests.get(
         f'{api_server}/api/users/server/{server_id}').json()
+    if (get_users['success'] == False):
+        print(get_users['error'])
+        return 0
+    get_users = get_users['data']
     # If loged in user is on the list of users then give him the roles from list
     db_user = {}
     for user in get_users:
         if user['username'] == member.name:
             db_user = user
+    if db_user == {}:
+        await logging_channel.send(f'{admin_role.mention} Unknown user: {str(member.mention)}')
+        return 0
     user_ranks = db_user['ranks']
     for rank in user_ranks:
         if rank not in guild.ranks:
