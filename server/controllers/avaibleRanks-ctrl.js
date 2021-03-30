8128ae7b678b92333810967ad6422faaf2e89204
Patryk Makowski <patryk.makowski23@gmail.com>
2021-03-30 20:13:37 +0200

    Add updating ranks for server

--- server/controllers/avaibleRanks-ctrl.js
+++ server/controllers/avaibleRanks-ctrl.js
@@ -10,3 +10,52 @@ getRanksByServerId = async (req, res) => {
     return res.status(200).json({ success: true, data: ranks });
   }).catch((error) => console.log(error));
 };
+
+// PUT /api/ranks/:id
+updateRanksForServer = async (req, res) => {
+  const body = req.body;
+
+  if (!body) {
+    return res.status(400).json({
+      success: false,
+      error: "You must provide a body to update",
+    });
+  }
+
+  await AvailableRanks.findOne(
+    { server_id: req.params.id },
+    (error, server) => {
+      if (error) {
+        return res.status(404).json({
+          error,
+          message: "Server not found",
+        });
+      }
+
+      const { name, server_id } = body;
+
+      if (name) {
+        server.name = name;
+      }
+      if (server_id) {
+        server.server_id = server_id;
+      }
+
+      server
+        .save()
+        .then(() => {
+          return res.status(200).json({
+            status: true,
+            id: server._id,
+            message: "Ranks updated",
+          });
+        })
+        .catch((error) => {
+          return res.status(404).json({
+            error,
+            message: "Ranks not updated",
+          });
+        });
+    }
+  );
+};
