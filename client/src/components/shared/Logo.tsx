7c1771b1cc1ca1af946f3c23576ac614249792da
Patryk Makowski <patryk.makowski23@gmail.com>
2021-04-02 15:27:53 +0200

    Create logo component

---
+++ client/src/components/shared/Logo.tsx
@@ -0,0 +1,26 @@
+import { css } from '@emotion/css';
+import { FC } from 'react';
+import logo from '../../images/logo.svg';
+import { underline } from '../../styles/underline';
+
+interface ILogo {
+  size: 'big' | 'small';
+}
+
+export const Logo: FC<ILogo> = ({ size }) => {
+  const Name = () => {
+    return (
+      <div className={css({ display: 'flex', alignItems: 'center', flexDirection: 'column' })}>
+        <h1 className={underline}>DISCORD</h1>
+        <h2>AUTO-ADDER</h2>
+      </div>
+    );
+  };
+
+  return (
+    <div>
+      <img src={logo} className={css({ width: size === 'big' ? '270px' : '110px' })} />
+      {size === 'big' && <Name />}
+    </div>
+  );
+};
